15484e6cb4d62ce39cf11b992285ebd3ee2bd6b4
SuryaChaitanyaBuse <sbuse1@asu.edu>
2026-03-30 09:54:02 -0700

    Demo for Nathan

--- scripts/config.py
+++ scripts/config.py
@@ -9,7 +9,7 @@ load_dotenv()
 
 NEO4J_URI      = os.getenv("NEO4J_URI",      "bolt://localhost:7687")
 NEO4J_USER     = os.getenv("NEO4J_USER",     "neo4j")
-NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD", "codex-password")
+NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD", "codexpassword")
 
 # Paths
 import os
